17455024826be644bb8f6e8cb7da026434ec0983
Davee Sok <daveesok@gmail.com>
2021-05-21 19:02:57 -0500

    changed title page

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,9 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
+        <h1>Davee Sok</h1>
+        <h3>Full Stack JavaScript Developer</h3>
+        <p> Node.js | Express.Js | React | Mongo </p>
       </div>
     </div>
     <nav>
